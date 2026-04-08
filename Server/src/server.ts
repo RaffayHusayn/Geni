9d22d57254e92c3aabed4fe89c46fa18045d3d93
raffay <raffaysyedd@gmail.com>
2026-04-08 15:56:11 -0400

    Add tsx instead of nodemon and a simple server file

---
+++ Server/src/server.ts
@@ -0,0 +1,21 @@
+import helmet from "helmet"
+import "dotenv/config" 
+import express, { Request, Response } from "express"
+import cors from "cors"
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+// ----  Middlewares ----
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/health', (req: Request, res: Response) => {
+  res.json({ status: 'ok' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
